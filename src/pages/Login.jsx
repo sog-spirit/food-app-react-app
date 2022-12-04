c1b5e468d0c46abf88157f0cbdc815bd2f31266d
sog-spirit <78286856+sog-spirit@users.noreply.github.com>
2022-12-04 14:01:40 +0700

    Set cookie with domain

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
     });
     let data = await result.json();
     if (data.detail == "Login successfully") {
-      Cookies.set('jwt', data.jwt)
+      Cookies.set('jwt', data.jwt, { domain: 'web-production-746d.up.railway.app' })
       await fetch(`${HOST}/api/user/view`, {
         headers: {
           'Authorization': `jwt=${data.jwt}`
